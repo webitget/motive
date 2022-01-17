0dd4a8fff5bb36cf9d019c0a8d8c1b5067e676ca
Garik <garik.28h@gmail.com>
2022-01-18 01:44:04 +0200

    Core initial commit

---
+++ packages/core/.size-limit.js
@@ -0,0 +1,12 @@
+module.exports = [
+    {
+        path: "dist/motive.cjs.production.min.js",
+        limit: "10 KB",
+        webpack: false,
+    },
+    {
+        path: "dist/motive.esm.js",
+        limit: "10 KB",
+        webpack: false,
+    },
+]
